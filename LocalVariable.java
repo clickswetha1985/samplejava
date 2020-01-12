a2a9eaffe2873df8bd1260b1a2623925ee60ecb3
clickswetha1985 <59783003+clickswetha1985@users.noreply.github.com>
2020-01-11 22:06:54 -0500

    Code changed in LocalVariable class

--- LocalVariable.java
+++ LocalVariable.java
@@ -1,27 +1,10 @@
-package sample;
 
 public class LocalVariable {
-	public int put;
-	public void putAge() {
-		int age = 0; //local variable
-        age = age + 6;
-        System.out.println("Dog age is : " + age);
-	}
 	
-	public int area()
-    {
-		
-        int length=10; //local variable
-        int breadth = 5; //local variable
-        int rectarea = length*breadth; //local variable
-        return rectarea;
-        
-        }
-
 	public static void main(String[] args) {
-		LocalVariable d = new LocalVariable();
+		
 		System.out.println(d.area()); //print 50
-		d.putAge(); //
+		
 
 	}
 
